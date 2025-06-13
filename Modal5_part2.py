3a591fb227dd11a4dada0e8aace2ef62e2329f45
Melissa Stovall <melissa.stovall@csuglobal.edu>
2025-06-13 09:53:56 -0500

    Adding suggestions form professor to improve program

--- Modal5_part2.py
+++ Modal5_part2.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
-num_books_purchased = int(input('please enter the number of books purchased this month\n'))
 
+#catches invalid inputs
+while True:
+    try:
+      num_books_purchased = int(input('please enter the number of books purchased this month\n')) 
+      if num_books_purchased < 0:
+        num_books_purchased = int(input('Input is invalid please enter number of books again\n'))
+      break #valid input,exit loop
+    except ValueError:
+        num_books_purchased = int(input('Input is invalid please enter number of books again\n'))
+
+
+  
+#functions to find the number of points  
 def findPurchasePoints(books):
+    
     if books >=2 and books < 4:
         purchase_points = 5
     elif books >= 4 and books < 6:
@@ -15,7 +28,10 @@ def findPurchasePoints(books):
         purchase_points = 0         
     return purchase_points
     
+
+#function call   
 points = findPurchasePoints(num_books_purchased)
+#outputs
 print('\nPoints Summary')
 print('Customer was awarded {} of points for purchasing {} books'.format(points,num_books_purchased))    
     
--- modal5_part1.py
+++ modal5_part1.py
@@ -2,6 +2,9 @@
 #getting user input for number of years
 num_years = int(input('Enter number of years to collect rainfall data\n'))
 
+#using a list of Months to tell the user what month they are entering data for
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September','October','November','December']
+
 #intiating rainfall total and number of months
 rainfall_total = 0 
 total_num_months = 0
@@ -10,7 +13,7 @@ total_num_months = 0
 if num_years > 0: 
     for i in range(0,num_years,1):  #loop though each year
         for j in range(0,12,1): #loop through each month of the year
-            rainfall = int(input('how much rain fell in month {} of year {}\n'.format(j+1,i+1)))
+            rainfall = int(input('how much rain fell in month {} of year {}\n'.format(months[j],i+1)))
             if rainfall > 0:
                 rainfall_total +=rainfall 
                 total_num_months += 1
